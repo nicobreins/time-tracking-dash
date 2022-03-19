b8cfc865e922084b2558ec672ba431e220d86497
nicobreins <bharatswaroop98@gmail.com>
2022-03-19 18:45:53 +0530

    add/data fetch

---
+++ dist/main.js
@@ -0,0 +1,42 @@
+const navLinks = document.querySelectorAll('.profile-nav-link')
+const currentTimeEls = document.querySelectorAll('.card-time');
+const prevTimeEls = document.querySelectorAll('.prev-time');
+
+
+function fetchData(period){
+    return fetch('../data.json')
+    .then(response => response.json())
+    .then(data => {
+        for(let i = 0; i < currentTimeEls.length; i++){
+            currentTimeEls[i].innerText = data[i].timeframes[period].current + 'hrs';
+            prevTimeEls[i].innerText = data[i].timeframes[period].previous + 'hrs';
+        }                    
+    })
+}
+
+fetchData('daily');
+
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (e)=>{
+        e.preventDefault;
+        const linkSiblings = Array.from(e.target.parentNode.children);
+        linkSiblings.forEach(linkk => {
+            linkk.classList.remove('active')
+        })
+        e.target.classList.add('active');
+
+        if(e.target.id == 'daily'){
+            fetchData('daily')
+        }
+
+        if(e.target.id == 'weekly'){
+            fetchData('weekly')
+        }
+
+        if(e.target.id == 'monthly'){
+            fetchData('monthly')
+        }
+        
+    });
+})
